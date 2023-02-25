aabbd0628c2a7e53793b11d9c6dccd0089660448
aravindmathradan <aravindmathradan2@gmail.com>
2023-02-25 19:21:19 +0530

    configured firebase admin cert and updated linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,106 @@
 module.exports = {
   env: {
-    es6: true,
+    es2021: true,
     node: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 'latest',
   },
   extends: [
-    "eslint:recommended"
+    'eslint:recommended'
   ],
-  rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
-  },
   overrides: [
     {
-      files: ["**/*.spec.*"],
+      files: ['**/*.spec.*'],
       env: {
         mocha: true,
       },
       rules: {},
-    },
+    }
   ],
   globals: {},
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ],
+    'quote-props': [
+      'warn',
+      'as-needed'
+    ],
+    'space-before-function-paren': [
+      'error',
+      'never'
+    ],
+    'comma-dangle': [
+      'warn',
+      {
+        arrays: 'never',
+        objects: 'always-multiline',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
+      }
+    ],
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'padded-blocks': [
+      'error',
+      'never'
+    ],
+    'one-var': [
+      'error',
+      {
+        initialized: 'never',
+        uninitialized: 'consecutive',
+      }
+    ],
+    'object-shorthand': 'warn',
+    'prefer-const': 'warn',
+    'no-var': 'warn',
+    'prefer-template': 'error',
+    'no-useless-escape': 'warn',
+    'wrap-iife': 'error',
+    'space-before-blocks': 'error',
+    'prefer-arrow-callback': 'warn',
+    'arrow-spacing': 'error',
+    'arrow-parens': 'error',
+    'implicit-arrow-linebreak': 'error',
+    'no-duplicate-imports': 'error',
+    'object-curly-newline': 'error',
+    'no-unused-vars': 'warn',
+    'comma-style': 'error',
+    'array-bracket-spacing': 'error',
+    'no-multiple-empty-lines': 'error',
+    'key-spacing': 'error',
+    'func-call-spacing': 'error',
+    'computed-property-spacing': 'error',
+    'comma-spacing': 'error',
+    'space-in-parens': 'error',
+    'newline-per-chained-call': 'warn',
+    'no-whitespace-before-property': 'error',
+    'eol-last': 'error',
+    'space-infix-ops': 'error',
+    'keyword-spacing': 'error',
+    'spaced-comment': 'error',
+    'no-else-return': 'warn',
+    'brace-style': 'error',
+    'nonblock-statement-body-position': 'error',
+    'no-case-declarations': 'error',
+    eqeqeq: 'warn',
+  },
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const admin = require('firebase-admin');
-const firebaseServiceAccountCert = require('./atrium-af29c-firebase-adminsdk-7jipo-f5aa02ab90.json');
+const firebaseServiceAccountCert = JSON.parse(process.env.FIREBASE_ADMIN_SDK_CERT);
 
 //  Express setup
 const app = express();
@@ -9,12 +9,12 @@ const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
 app.use(express.json({ limit: '1mb' }));
 
-app.get('/', (req, res) => {
-  res.send("Atrium server is running...");
+app.get('/status', (req, res) => {
+  res.send('Atrium server is running...');
 });
 
 //  Firestore setup
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseServiceAccountCert)
+  credential: admin.credential.cert(firebaseServiceAccountCert),
 });
 const db = admin.firestore();
